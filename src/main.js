32a10efbb87e57dcb4bdccf670fbfe8b4c74222a
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-03-16 03:22:45 +0330

    create menu section

--- src/main.js
+++ src/main.js
@@ -16,3 +16,51 @@ hamburger.addEventListener('click',()=>{
 // navClose.addEventListener('click',()=>{
 //     navMenu.classList.add('hidden')
 // })
+
+
+
+// TABS-------- 
+
+const tabs=document.querySelectorAll('.tabs_wrap ul li')
+const all=document.querySelectorAll('.item_wrap')
+const foods=document.querySelectorAll('.food')
+const beverages=document.querySelectorAll('.beverage')
+const snacks=document.querySelectorAll('.snack')
+
+tabs.forEach((tab)=>{
+    tab.addEventListener('click',()=>{
+        tabs.forEach((tab)=>{
+            tab.classList.remove('active')
+        })
+
+        tab.classList.add('active')
+
+        
+
+        const tabval=tab.getAttribute('data-tabs')
+
+        all.forEach((item)=>{
+            item.style.display='none'
+        })
+
+        if(tabval==='food'){
+            foods.forEach((food)=>{
+                food.style.display='block'
+            })
+        }else if(tabval==='snack'){
+            snacks.forEach((snack)=>{
+                snack.style.display='block'
+            })
+        }
+        else if(tabval==='beverage'){
+            beverages.forEach((beverage)=>{
+                beverage.style.display='block'
+            })
+        }else{
+            all.forEach((item)=>{
+                item.style.display='block'
+            })
+        }
+
+    })
+})
